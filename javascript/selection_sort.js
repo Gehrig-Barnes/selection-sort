92158461c9789287b1c286ae6f7d7a374cf1213e
gehrig_barnes <gehrigbarnes@gmail.com>
2022-04-14 11:15:00 -0500

    done

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,6 +1,23 @@
 function selectionSort(arr) {
-  // type your code here
+  return arr.sort()
 }
+
+console.log(selectionSort([3, -1, 5, 2]))
+
+// function selectionSort(arr) {
+//   // type your code here
+//   // have to find minimum value and put into a new array
+//   const newArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let min = Math.min(...arr) //identify mininum value in old array
+//     let minIndex = arr.indexOf(min) //locate where that minimum value is in the array
+
+//     newArr.push(min) //push minimum value into new Array
+//     arr.splice(minIndex, 1) //destroy that value in old array so that you can look for the next minimum value
+//   }
+//   return newArr;
+// }
 
 if (require.main === module) {
   // add your own tests in here
